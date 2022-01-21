cbb00276f6f7aba671f6dabc9a64a5cae7799673
Hitesh Arora <hitesharora@Hiteshs-MacBook-Pro.local>
2022-01-21 14:28:40 -0500

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,12 +6,13 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log(`tell me`)
-    const location = search.value
+    const location = search.value;
+
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             console.log(data)
             if (data.error) {
